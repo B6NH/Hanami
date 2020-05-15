0683218bd88c0a9ccd481577d8aad1b265151474
Jan Kowalski <yakov85@o2.pl>
2020-05-15 23:43:07 +0200

    Fix Tests

--- apps/web/controllers/singers/create.rb
+++ apps/web/controllers/singers/create.rb
@@ -4,8 +4,6 @@ module Web
       class Create
         include Web::Action
 
-        expose :singer
-
         params do
           required(:singer).schema do
             required(:name).filled(:str?)
@@ -14,7 +12,7 @@ module Web
 
         def call(params)
           if params.valid?
-            @book = SingerRepository.new.create(params[:singer])
+            SingerRepository.new.create(params[:singer])
             redirect_to routes.singers_path
           else
             self.status = 422
--- spec/web/controllers/songs/create_spec.rb
+++ spec/web/controllers/songs/create_spec.rb
@@ -1,9 +1,19 @@
 RSpec.describe Web::Controllers::Songs::Create, type: :action do
   let(:action) { described_class.new }
-  let(:params) { Hash[] }
+  let(:repository) { SingerRepository.new }
+  let(:song_repository) { SongRepository.new }
+  let(:singer_params) { Hash[singer:{ name:'Katie Melua' }] }
+  let(:params) { Hash[song:{ singer_id: 1, title: 'Moonshine' }] }
 
   it 'is successful' do
+    singer = repository.create(singer_params[:singer])
     response = action.call(params)
-    expect(response[0]).to eq 200
+
+    song = song_repository.last
+
+    expect(song.id).to_not be_nil
+    expect(song.title).to eq(params.dig(:song, :title))
+
+    expect(response[0]).to eq 302
   end
 end
